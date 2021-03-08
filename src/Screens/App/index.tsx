6e75befa459bd4e92b2f1183646d10c5e4747d7e
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-09 00:49:59 +0100

    Central export for App screens

---
+++ src/Screens/App/index.tsx
@@ -0,0 +1,7 @@
+import Home from './Home';
+import Book from './Book';
+import BookMark from './BookMark';
+import Cart from './Cart';
+import Settings from './Settings';
+
+export {Home,Book, BookMark, Cart, Settings}
