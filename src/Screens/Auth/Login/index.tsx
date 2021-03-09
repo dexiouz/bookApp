11b17bec01fa5d614982eeec54e5e70bd4e8efd4
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-09 02:32:43 +0100

    Create Login screen

--- src/Screens/Auth/Login/index.tsx
+++ src/Screens/Auth/Login/index.tsx
@@ -1,18 +1,23 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+import {styles} from './styles';
+import LoginForm from './LoginForm';
 interface LoginProps {}
 
 const Login = (props: LoginProps) => {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <View style={{flex: 1.1, justifyContent: 'flex-end'}}>
+        <View style={{width: '50%', marginLeft: 20, marginBottom: 70}}>
+          <Text style={styles.adventureText}>Adventures </Text>
+          <Text style={styles.adventureText}>in literature</Text>
+          <Text style={{color: '#fff', marginTop: 25}}>Join Our Community</Text>
+        </View>
+      </View>
+
+      <LoginForm />
     </View>
   );
 };
 
 export default Login;
-
-const styles = StyleSheet.create({
-  container: {}
-});
