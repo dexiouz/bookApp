fee964ff2c78e89d767974ac58e06a8995f944b8
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-09 16:10:43 +0100

    Export screens

--- src/Screens/App/index.tsx
+++ src/Screens/App/index.tsx
@@ -3,5 +3,5 @@ import Book from './Book';
 import BookMark from './BookMark';
 import Cart from './Cart';
 import Settings from './Settings';
-
-export {Home,Book, BookMark, Cart, Settings}
+import BookDetails from './BookDetails';
+export {Home,Book, BookMark, Cart, Settings, BookDetails}
