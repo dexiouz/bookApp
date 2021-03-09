0fc2368c52d8e5e8cac1678138218bd830131570
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-10 00:27:03 +0100

    Updated screens

--- src/Screens/App/index.tsx
+++ src/Screens/App/index.tsx
@@ -4,4 +4,5 @@ import BookMark from './BookMark';
 import Cart from './Cart';
 import Settings from './Settings';
 import BookDetails from './BookDetails';
-export {Home,Book, BookMark, Cart, Settings, BookDetails}
+import PreviewPage from './PreviewPage';
+export {Home,Book, BookMark, Cart, Settings, BookDetails, PreviewPage}
