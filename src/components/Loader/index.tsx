b93449b50694449360dc3c038dc332e3cc5ce262
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-09 02:32:23 +0100

    Created the loader component

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import {ActivityIndicator, View} from 'react-native';
 import {styles} from './styles';
- const index = ({otherColor, size}) => (
+
+interface Props{
+  otherColor: string,
+  size: number
+}
+ const index: React.FC<Props> = ({otherColor, size}) => (
   <View style={styles.container}>
     <ActivityIndicator
      size={size ? size : "large"}
