1c78f0887b51b553df19bddad3531462fa4a86b5
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-09 14:28:45 +0100

    styles for Home

---
+++ src/Screens/App/Home/styles.tsx
@@ -0,0 +1,10 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 15, 
+    paddingBottom: 20
+    // backgroundColor: "#E5E5E5"
+  }
+});
