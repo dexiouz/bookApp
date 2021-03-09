027a4cc62c78834c0fa0662d7e95f3404da099eb
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-10 00:28:13 +0100

    Popular books receives dynamic data

--- src/Screens/App/Home/PopularBooks/index.tsx
+++ src/Screens/App/Home/PopularBooks/index.tsx
@@ -1,22 +1,46 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, FlatList} from 'react-native';
+import {Loader} from '../../../../components';
+import {popularBooksUrl} from '../../../../api';
+import axios from 'axios';
 import PopularBooksCard from './popularBooksCard';
 import {styles} from './styles';
+const url = popularBooksUrl;
 interface HomeProps {}
-const data = [1, 2, 4, 5, 6, 7, 7, 8, 8];
-const Home = (props: HomeProps) => {
+const index: React.FC<HomeProps> = () => {
+  const [state, setState] = React.useState<[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    const getBooks = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setState(res.data.items);
+      } catch (e) {
+        console.log(e, 'error');
+      }
+      setLoading(false);
+    };
+
+    getBooks();
+  }, []);
+
   return (
     <View>
       <Text style={styles.title}>Popular Books</Text>
-      <FlatList
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        data={data}
-        renderItem={({item}) => <PopularBooksCard />}
-        keyExtractor={(item) => Math.random().toString()}
-      />
+      {loading ? (
+        <Loader />
+      ) : (
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={state}
+          renderItem={({item}) => <PopularBooksCard item={item} />}
+          keyExtractor={(item) => Math.random().toString()}
+        />
+      )}
     </View>
   );
 };
 
-export default Home;
+export default index;
--- src/Screens/App/Home/PopularBooks/popularBooksCard.tsx
+++ src/Screens/App/Home/PopularBooks/popularBooksCard.tsx
@@ -2,35 +2,51 @@ import * as React from 'react';
 import {Text, View, Image, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import {styles} from './styles';
-interface HomeProps {}
+interface HomeProps {
+  item: any
+}
 
-const Home = (props: HomeProps) => {
+const index:React.FC<HomeProps> = ({item}) => {
+  
   const navigation = useNavigation();
+  const title = item?.volumeInfo?.title || '';
+  const author = item?.volumeInfo?.authors || '';
+  const image = item?.volumeInfo?.imageLinks?.smallThumbnail || '';
+  const rating = item?.volumeInfo?.averageRating || '';
+  const amount = item?.saleInfo?.listPrice?.amount || 'Free';
+  const realAuthor = author[0] !== undefined ? author[0] : '';
+  const useAuthorName = realAuthor ? realAuthor.slice(0, 19) : realAuthor;
+  const link = item?.selfLink; 
+  console.log(item.saleInfo, 'am')
   return (
-    <TouchableOpacity onPress={()=>navigation.navigate("BookDetails")}>
+    <TouchableOpacity onPress={()=>navigation.navigate("BookDetails", {
+      link
+    })}>
       <View
         style={{
           backgroundColor: '',
-          width: 120,
+          width: 140,
           marginLeft: 10,
           borderRadius: 10,
         }}>
         <View style={{backgroundColor: '', width: '100%', height: 192}}>
-          <Image
-            source={require('../../../../assets/images/modified.png')}
+          <Image 
+            source={{
+              uri: image,
+            }}
             style={{width: '100%', height: '100%'}}
             resizeMode="cover"
           />
         </View>
         <View>
-          <Text style={{marginVertical: 5, fontSize: 16, fontWeight: '600'}}>
-            Fashionopolis
+          <Text style={{marginVertical: 5, fontSize: 14, fontWeight: '600'}}>
+            {title.slice(0, 30) || ''}
           </Text>
-          <Text style={{fontSize: 12}}>Patrick Mauriès</Text>
+          <Text style={{fontSize: 12}}>{useAuthorName || ''}</Text>
         </View>
       </View>
     </TouchableOpacity>
   );
 };
 
-export default Home;
+export default index;
