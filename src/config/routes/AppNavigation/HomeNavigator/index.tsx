304573ce6643686401be2b7a4bca0f74636f0da2
Chidera Ugwuanyi <dexiouz@gmail.com>
2021-03-10 00:26:48 +0100

    Updated navigators

--- src/config/routes/AppNavigation/HomeNavigator/index.tsx
+++ src/config/routes/AppNavigation/HomeNavigator/index.tsx
@@ -3,11 +3,12 @@ import {createStackNavigator} from '@react-navigation/stack';
 const App = createStackNavigator();
 
 //import Auth Screens;
-import {BookDetails, Home} from '../../../../Screens/App'
+import {BookDetails, Home, PreviewPage} from '../../../../Screens/App'
 const AuthStackScreens = () => (
   <App.Navigator headerMode="none" initialRouteName="Home">
     <App.Screen name="Home" component={Home} /> 
     <App.Screen name="BookDetails" component={BookDetails} /> 
+    <App.Screen name="PreviewPage" component={PreviewPage} /> 
   </App.Navigator>
 );
 
