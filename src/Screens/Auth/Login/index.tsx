abc41f60af5a02a0404a4f1ee82f285ffa81afb0
Chidera Paul Ugwuanyi <dexiouz@gmail.com>
2021-03-10 23:41:46 +0100

    Set up fingerprint auth

--- src/Screens/Auth/Login/index.tsx
+++ src/Screens/Auth/Login/index.tsx
@@ -1,52 +1,54 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {View} from 'react-native';
-import FingerprintScanner from 'react-native-fingerprint-scanner';
+import React, {useEffect, useContext} from 'react';
+import {View} from 'react-native'; 
+import TouchID from 'react-native-touch-id';
 import {AuthContext} from '../../../context/authContext';
 import {styles} from './styles';
 import LoginForm from './LoginForm';
 import Top from './top';
 interface LoginProps {}
 
-const Login = (props: LoginProps) => {
-  console.log(FingerprintScanner, 'FingerprintScanner');
-  const [biometryType, setBiometryType] = useState<any>(null);
+const Login = (props: LoginProps) => { 
   const {signIn} = useContext(AuthContext); 
-  const getMessage = () => {
-    if (biometryType == 'Face ID') {
-      return 'Scan your Face on the device to continue';
-    } else {
-      return 'Scan your Fingerprint on the device scanner to continue';
-    }
-  };
-  const showAuthenticationDialog =  () => {
-    if (biometryType !== null && biometryType !== undefined) {
-      FingerprintScanner.authenticate({
-        description: getMessage(),
+    const _pressHandler = () => {
+    TouchID.authenticate('Scan your Fingerprint on the device scanner to continue', optionalConfigObject)
+      .then(() => {
+        signIn("dex@g.co", 'eurn48209ojsnsdj'); 
       })
-        .then(async() => {
-          signIn("dex@g.co", 'eurn48209ojsnsdj');
-          console.log('object')
-        })
-        .catch((error) => {
-          console.log('Authentication error is => ', error);
-        });
-    } else {
-      console.log('biometric authentication is not available');
-    }
+      .catch((error: string) => {
+        console.log(error, 'error')
+      });
+  }
+  const optionalConfigObject = {
+    title: 'Authentication Required', // Android
+    imageColor: '#e00606', // Android
+    imageErrorColor: '#ff0000', // Android
+    sensorDescription: 'Touch sensor', // Android
+    sensorErrorDescription: 'Failed', // Android
+    cancelText: 'Cancel', // Android
+    fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
+    unifiedErrors: false, // use unified error messages (default false)
+    passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
   };
   useEffect(() => {
-    FingerprintScanner.isSensorAvailable()
-      .then((biometryType) => {
-        setBiometryType(biometryType);
-      })
-      .catch((error) => console.log('isSensorAvailable error => ', error));
+    TouchID.isSupported(optionalConfigObject)
+  .then(biometryType => {
+    // Success code
+    if (biometryType === 'FaceID') {
+        console.log('FaceID is supported.');
+    } else {
+        console.log('TouchID is supported.');
+    }
+  })
+  .catch(error => {
+    // Failure code
+    console.log(error);
+  });
   }, []); 
   return (
     <View style={styles.container}>
       <Top />
-      <LoginForm fingerAuth={showAuthenticationDialog} />
+      <LoginForm fingerAuth={_pressHandler} />
     </View>
   );
 };
-
 export default Login;
