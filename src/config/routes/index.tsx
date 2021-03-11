20a4c52e597cacd1b0b3257d69445627c234e21f
Chidera Paul Ugwuanyi <dexiouz@gmail.com>
2021-03-11 01:01:31 +0100

    Set up netinfo to check network connectivity

--- src/config/routes/index.tsx
+++ src/config/routes/index.tsx
@@ -1,26 +1,17 @@
 import React, {useMemo, useEffect, useReducer, useState} from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {AuthContext} from '../../context/authContext';
 import {NavigationContainer} from '@react-navigation/native';  
 import {createStackNavigator} from '@react-navigation/stack';
-const AuthStack = createStackNavigator();
-//import Auth Screens
 import AuthStackScreens from './AuthNavigation';
-//import App Screens
 import AppStackScreens from './AppNavigation';
 import {ActivityIndicator} from 'react-native';
-// import ShowMessage, {type} from '../../components/toaster/ShowMessage';
-import {Alert} from 'react-native';
-// import Error from '../../components/ErrorComponent';
 import {useNetInfo} from '@react-native-community/netinfo';
-// import Empty from '../../components/EmptyComponent';
 import Entypo from 'react-native-vector-icons/Entypo';
-
+import {Empty} from '../../components';
 const AppRoute = () => {
-  // const {signOut} = useContext(AuthContext);
   const netInfo = useNetInfo();
-  // const [logout, {loading}] = useMutation(LOGOUT);
   const handleSignOut = async () => {
     try {
       await AsyncStorage.clear();
@@ -101,23 +92,10 @@ const AppRoute = () => {
     );
   }
 
-  // const login = true;
-
   return (
     <AuthContext.Provider value={authContext}>
-      <> 
-          <NavigationContainer>
-            {loginState.userToken !== null ? (
-              <>
-              <AppStackScreens /> 
-              </>
-            ) : (
-              <AuthStackScreens />
-            )}
-          </NavigationContainer>
-     
-      </>
-      {/* <>
+       <>
+       {/* check if device is connected to internet */}
         {netInfo.isConnected.toString() === 'true' ? (
           <NavigationContainer>
             {loginState.userToken !== null ? (
@@ -132,7 +110,7 @@ const AppRoute = () => {
             networkIcon={<Entypo name="line-graph" color="red" size={38} />}
           />
         )}
-      </> */}
+      </>
     </AuthContext.Provider>
   );
 };
