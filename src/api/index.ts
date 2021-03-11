704eb570e5e12067f6f5dd2f4a9342067f581016
Chidera Paul Ugwuanyi <dexiouz@gmail.com>
2021-03-11 08:59:25 +0100

    Made api more dynamic

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,7 @@
- export const popularBooksUrl = 'https://www.googleapis.com/books/v1/volumes?q=google';
- export const newBooksUrl = 'https://www.googleapis.com/books/v1/volumes?q=mathematics';
+ const popKeyWords = ['google', 'facebook', 'amazon', 'social', 'technology', 'engineering']
+ const newKeyWords = ['physics', 'code', 'programmming', 'mathematics', 'genetics', 'water'];
+ const t = Math.floor(Math.random() * 6) + 1  ;
+
+ 
+ export const popularBooksUrl = `https://www.googleapis.com/books/v1/volumes?q=${popKeyWords[t]}`;
+ export const newBooksUrl = `https://www.googleapis.com/books/v1/volumes?q=${newKeyWords[t]}`;
